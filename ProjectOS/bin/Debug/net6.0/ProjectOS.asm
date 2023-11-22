2d143bbc02cfeb82e6283010608aba012ff437b9
sleep-deepfried <pulidoearljohn14@gmail.com>
2023-11-23 00:24:01 +0800

    fixed the dependencies

--- ProjectOS/bin/Debug/net6.0/ProjectOS.asm
+++ ProjectOS/bin/Debug/net6.0/ProjectOS.asm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e371bc6e57ebe8b599144042b741ec57f2e43537f3adc96de976763e72e7bbdc
-size 76253189
+oid sha256:09e9c7301eb7169ad81b997e57fd1932c3bc4b866203f86434aacf07fed3ab64
+size 76251626
